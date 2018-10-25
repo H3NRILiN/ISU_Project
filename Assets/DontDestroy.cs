02027f22ebd483ee6f496725b501dfdbae6fe58e
H3nriLiN <so7788520@gmail.com>
2018-10-25 20:39:07 +0800

    更新

---
+++ Assets/DontDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+    public Transform Follower;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Update()
+    {
+        transform.position = Follower.position;
+    }
+}
